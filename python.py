4fd476b4b6af97a727b853e75d3caca450431a68
Sudhanshukulkarni50 <sudhanshu.kulkarni50@gmail.com>
2024-12-09 11:15:02 +0530

    Add files via upload

---
+++ python.py
@@ -0,0 +1,54 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 3,
+   "id": "38352420-42c2-407a-814e-374b2ab60940",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "the sum of a and b is:  3\n"
+     ]
+    }
+   ],
+   "source": [
+    "a=1\n",
+    "b=2\n",
+    "sum = a+b\n",
+    "print(\"the sum of a and b is: \",sum)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "95ad713f-9e0f-4c7d-b76d-83b679c33597",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.13.1"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
